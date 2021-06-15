91b3fd92269ab443ce08cd25fad22f1662796ddd
Michael Nnaji <mnnaji3@uwo.ca>
2021-06-15 22:24:23 +0000

    commit tree 9adbe2d3f62182aaf3855b5e7da43a9cfa7f1427

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your code in this file!
-const currentUser = "Michael";
-const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
-const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + "!";
-const shortGreeting = "Welcome, " + currentUser[0] + "!";
+const currentUser = `Michael`;
+const welcomeMessage = `Welcome to Flatbook, ${currentUser} !`;
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()} !`;
+const shortGreeting = `Welcome, ${currentUser[0]} !`;
